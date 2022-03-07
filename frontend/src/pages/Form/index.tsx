a78fa3c542df6de82b0c736caa73fde662fdf284
Larissa Roberta <larissa.roberta569@hotmail.com>
2022-03-07 14:49:04 -0300

    Form layout

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,7 +1,42 @@
+import "./style.css"
+
 function Form(){
 
+    const movie = {
+        id: 1,
+        image: "https://p2.trrsf.com/image/fget/cf/648/0/images.terra.com/2021/12/21/29156024-the-witcher.jpg",
+        title: "The Witcher",
+        count: 2,
+        score: 4.5
+    };
+
     return(
-        <h1>Pagina de Formulário</h1>
+        <div className="dsmovie-form-container">
+    <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
+    <div className="dsmovie-card-bottom-container">
+        <h3>{movie.title}</h3>
+        <form className="dsmovie-form">
+            <div className="form-group dsmovie-form-group">
+                <label htmlFor="email">Informe seu email</label>
+                <input type="email" className="form-control" id="email" />
+            </div>
+            <div className="form-group dsmovie-form-group">
+                <label htmlFor="score">Informe sua avaliação</label>
+                <select className="form-control" id="score">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                </select>
+            </div>
+            <div className="dsmovie-form-btn-container">
+                <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
+            </div>
+        </form >
+        <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+    </div >
+</div >
     );
     
 }
