ce76ba898096c4a866187e6830e4d76a58cbe16f
Larissa Roberta <larissa.roberta569@hotmail.com>
2022-03-07 14:22:20 -0300

    NavBar

---
+++ frontend/src/components/NavBar/index.tsx
@@ -0,0 +1,21 @@
+import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
+import "./styles.css";
+
+function NavBar(){
+    return (
+        <header>
+          <nav className="container">
+            <div className="dsmovie-nav-content">
+              <h1>DSMovie</h1>
+              <a href="https://github.com/lroberta569"/>
+              <div className="dsmovie-contact-container">
+                < GithubIcon />
+                <p className="dsmovie-contact-link">/lroberta569</p>
+              </div>
+            </div>
+          </nav>
+        </header>
+      );
+}
+
+export default NavBar;
