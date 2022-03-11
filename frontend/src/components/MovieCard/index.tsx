75e9120b297a1ba6348818d2ec925f90ae8dfd36
Larissa Roberta <larissa.roberta569@hotmail.com>
2022-03-11 17:34:37 -0300

    Corrigindo Erro

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 function MovieCard() {
 
-    const movie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
+  const movie = {
+    id: 1,
+    image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+    title: "The Witcher",
+    count: 2,
+    score: 4.5,
+};
 
   return (
     <div>
