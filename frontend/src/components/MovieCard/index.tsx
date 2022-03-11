f436d332ce619448fd77f6f2d65b4a0b02b7f0a3
Larissa Roberta <larissa.roberta569@hotmail.com>
2022-03-11 17:26:53 -0300

    Corrigindo Erro

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -8,16 +8,12 @@ function MovieCard() {
         image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
         title: "The Witcher",
         count: 2,
-        score: 4.5,
+        score: 4.5
     };
 
   return (
     <div>
-      <img
-        className="dsmovie-movie-card-image"
-        src={movie.image}
-        alt={movie.title}
-      />
+      <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title}/>
       <div className="dsmovie-card-bottom-container">
         <h3>{movie.title}</h3>
         <MovieScore />
@@ -29,4 +25,5 @@ function MovieCard() {
     </div>
   );
 }
+
 export default MovieCard;
