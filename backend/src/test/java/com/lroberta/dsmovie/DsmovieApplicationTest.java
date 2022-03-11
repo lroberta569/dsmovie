83e40eb76798fd8b7d7ea270c2843fe7cef777ec
Larissa Roberta <larissa.roberta569@hotmail.com>
2022-03-11 12:40:44 -0300

    Domain model, database seed

---
+++ backend/src/test/java/com/lroberta/dsmovie/DsmovieApplicationTest.java
@@ -0,0 +1,14 @@
+package com.lroberta.dsmovie;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DsmovieApplicationTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
