3ba207734da871c884231bc10050491c55ef2f0d
Larissa Roberta <larissa.roberta569@hotmail.com>
2022-03-12 10:12:49 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 import { BASE_URL } from "utils/requests";
@@ -7,42 +7,48 @@ import { MoviePage } from "types/movie";
 
 function Listing() {
 
-  const [pageNumber, setPageNumber] = useState(0);
+    const [pageNumber, setPageNumber] = useState(0);
 
-  const [page, setPage] = useState<MoviePage>({
-    content: [],
-    last: true,
-    totalPages: 0,
-    totalElements: 0,
-    size: 12,
-    number: 0,
-    first: true,
-    numberOfElements: 0,
-    empty: true
-  });
-
-  useEffect(() => {
-    axios.get(`${BASE_URL}/movies?size=12$page=${pageNumber}&sort=title`).then(response => {
-      const data = response.data as MoviePage;
-      setPage(data);
+    const [page, setPage] = useState<MoviePage>({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true
     });
-  }, [pageNumber]);
 
-  return (
-    <>
-      <Pagination />
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
+            .then(response => {
+                const data = response.data as MoviePage;
+                setPage(data);
+            });
+    }, [pageNumber]);
 
-      <div className="container">
-        <div className="row">
-          {page.content.map(movie => (
-            <div key={movie.id} className="col-sm-6 col-lg-4 col-lx-3">
-              <MovieCard movie={movie} />
+    const handlePageChange = (newPageNumber : number) => {
+        setPageNumber(newPageNumber);
+    }
+
+    return (
+        <>
+            <Pagination page={page} onChange={handlePageChange} />
+
+            <div className="container">
+                <div className="row">
+                    {page.content.map(movie => (
+                        <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                            <MovieCard movie={movie} />
+                        </div>
+                    )
+                    )}
+                </div>
             </div>
-          )
-          )}
-        </div>
-      </div>
-      </>
+        </>
     );
 }
-      export default Listing;
+
+export default Listing;
