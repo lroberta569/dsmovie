70fc9747cb7e5bb329dd7f9b9d413c3f5c845d35
Larissa Roberta <larissa.roberta569@hotmail.com>
2022-03-12 10:45:12 -0300

    Corrindo erro

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -2,7 +2,6 @@ import axios, { AxiosRequestConfig } from "axios";
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Movie } from "types/movie";
-import { textChangeRangeIsUnchanged } from "typescript";
 import { BASE_URL } from "utils/requests";
 import { validateEmail } from "utils/validate";
 import "./style.css"
